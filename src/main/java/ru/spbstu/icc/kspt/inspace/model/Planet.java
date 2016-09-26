7681e137da00bee43c3a3d0166d3cc79c42ea208
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2016-09-26 22:10:19 +0300

    add resources

--- src/main/java/ru/spbstu/icc/kspt/inspace/model/Planet.java
+++ src/main/java/ru/spbstu/icc/kspt/inspace/model/Planet.java
@@ -1,4 +1,14 @@
 package ru.spbstu.icc.kspt.inspace.model;
 
 public class Planet {
+
+    private Resources resources;
+
+    public Planet() {
+        this.resources = new Resources(0,0,0);
+    }
+
+    public void update() {
+
+    }
 }
---
+++ src/main/java/ru/spbstu/icc/kspt/inspace/model/Resources.java
@@ -0,0 +1,41 @@
+package ru.spbstu.icc.kspt.inspace.model;
+
+public class Resources {
+    private int metal;
+    private int crystals;
+    private int deuterium;
+
+    public Resources(int metal, int crystals, int deuterium) {
+        this.metal = metal;
+        this.crystals = crystals;
+        this.deuterium = deuterium;
+    }
+
+    public void addResources(Resources resources) {
+        metal += resources.metal;
+        crystals += resources.crystals;
+        deuterium += resources.deuterium;
+    }
+
+    public Resources getResources(int metal, int crystals, int deuterium) {
+
+        int metalAmount = (metal <= this.metal) ? metal : this.metal;
+        int crystalsAmount = (crystals <= this.metal) ? crystals : this.metal;
+        int deuteriumAmount = (deuterium <= this.metal) ? deuterium : this.metal;
+
+        this.metal -= metalAmount;
+        this.crystals -= crystalsAmount;
+        this.deuterium -= deuteriumAmount;
+
+        return new Resources(metalAmount, crystalsAmount, deuteriumAmount);
+    }
+
+    @Override
+    public String toString() {
+        return "Resources{" +
+                "Metal:" + metal +
+                ", Crystals: " + crystals +
+                ", Deuterium: " + deuterium +
+                '}';
+    }
+}
