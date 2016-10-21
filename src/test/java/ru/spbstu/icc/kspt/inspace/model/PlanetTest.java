6eb82f315510f31c8cc1d4f514ac97b48fff3471
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2016-10-21 23:28:45 +0300

    add test with powermockito

--- src/test/java/ru/spbstu/icc/kspt/inspace/model/PlanetTest.java
+++ src/test/java/ru/spbstu/icc/kspt/inspace/model/PlanetTest.java
@@ -1,13 +1,26 @@
 package ru.spbstu.icc.kspt.inspace.model;
 
 
-import org.junit.Test;
-import ru.spbstu.icc.kspt.inspace.model.buildings.Building;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.api.mockito.PowerMockito;
+import ru.spbstu.icc.kspt.inspace.model.buildings.Building;
+import ru.spbstu.icc.kspt.inspace.model.utils.Time;
+
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.*;
 import static ru.spbstu.icc.kspt.inspace.model.Planet.BuildingType;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.Map;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Time.class)
 public class PlanetTest {
 
     private Planet planet = new Planet("Nibiru");
@@ -15,20 +28,18 @@ public class PlanetTest {
     @Test
     public void testUpdate() {
 
-        System.out.println(planet.getResources());
+        assertEquals(planet.getResources(), new Resources(0, 0, 0));
 
-        try {
-            Thread.sleep(60000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        PowerMockito.mockStatic(Time.class);
+        when(Time.now()).thenReturn(LocalDateTime.now().plus(Duration.ofMinutes(531)));
 
-        planet.update();
-        System.out.println(planet.getResources());
+        assertEquals(planet.getResources(), new Resources(266, 221, 177));
     }
 
     @Test
     public void testGetBuildings() {
+        PowerMockito.mockStatic(Time.class);
+        when(Time.now()).thenReturn(LocalDateTime.now().plus(Duration.ofMinutes(531)));
         System.out.println("Planet: " + planet.getName());
         System.out.println("Resources: " + planet.getResources());
         System.out.println("Energy level: " + planet.getEnergyLevel());
@@ -43,5 +54,25 @@ public class PlanetTest {
             System.out.println("Duration: " + building.getUpgradeDuration());
             System.out.println();
         }
+    }
+
+    @Test
+    public void testUpgrade() {
+
+        PowerMockito.mockStatic(Time.class);
+        when(Time.now()).thenReturn(LocalDateTime.now().plus(Duration.ofMinutes(531)));
+
+        Building building = planet.getBuilding(BuildingType.FACTORY);
+        assertTrue(building.canBeUpgraded());
+        assertEquals(building.getLevel(), 0);
+        building.startUpgrade();
+
+        PowerMockito.mockStatic(Time.class);
+        when(Time.now()).thenReturn(LocalDateTime
+                .now()
+                .plus(Duration.ofMinutes(532))
+                .plus(building.getUpgradeDuration()));
+        planet.update();
+        assertEquals(building.getLevel(), 1);
     }
 }
