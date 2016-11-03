bfefee8f42afd58105d2d3f05a3663bfbb6a53f9
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2016-11-03 20:53:10 +0300

    add building and research for ships

---
+++ src/main/java/ru/spbstu/icc/kspt/inspace/model/buildings/Shipyard.java
@@ -0,0 +1,39 @@
+package ru.spbstu.icc.kspt.inspace.model.buildings;
+
+
+import ru.spbstu.icc.kspt.inspace.model.Resources;
+
+import java.time.Duration;
+
+public class Shipyard extends Building {
+
+    private static final int METAL_COST_VALUE = 60;
+    private static final int CRYSTAL_COST_VALUE = 50;
+    private final static int UPGRADE_SPEED_VALUE = 2900;
+
+    private Factory factory;
+
+    public Shipyard(BuildingDepartment department) {
+        super(department);
+    }
+
+    @Override
+    public void updateDependencies() {
+        factory = (Factory)department.getBuilding(BuildingType.FACTORY);
+    }
+
+    @Override
+    public Duration getUpgradeDuration() {
+        Resources cost = getUpgradeCost();
+        double summaryCost = cost.getMetal() + cost.getCrystals() + cost.getMetal();
+        double hours = summaryCost / (UPGRADE_SPEED_VALUE * (1 + factory.getLevel()));
+        return Duration.ofSeconds(Math.round(hours * 3600));
+    }
+
+    @Override
+    public Resources getUpgradeCost() {
+        int metal = (int)Math.round(METAL_COST_VALUE * Math.pow(1.6, getLevel()));
+        int crystal = (int)Math.round(CRYSTAL_COST_VALUE * Math.pow(1.6, getLevel()));
+        return new Resources(metal, crystal, 0);
+    }
+}
---
+++ src/main/java/ru/spbstu/icc/kspt/inspace/model/research/LaserTechnology.java
@@ -0,0 +1,42 @@
+package ru.spbstu.icc.kspt.inspace.model.research;
+
+import ru.spbstu.icc.kspt.inspace.model.Resources;
+import ru.spbstu.icc.kspt.inspace.model.buildings.BuildingType;
+import ru.spbstu.icc.kspt.inspace.model.buildings.Laboratory;
+
+import java.time.Duration;
+
+public class LaserTechnology extends Research {
+
+    private static final int METAL_COST_VALUE = 45;
+    private static final int CRYSTAL_COST_VALUE = 65;
+    private static final int DEUTERIUM_COST_VALUE = 50;
+    private static final int UPGRADE_SPEED_VALUE = 1000;
+
+    private Laboratory laboratory;
+
+    public LaserTechnology(ResearchDepartment department) {
+        super(department);
+    }
+
+    @Override
+    public Duration getUpgradeDuration() {
+        Resources cost = getUpgradeCost();
+        double summaryCost = cost.getMetal() + cost.getCrystals() + cost.getDeuterium();
+        double hours = summaryCost / (UPGRADE_SPEED_VALUE * (1 + laboratory.getLevel()));
+        return Duration.ofSeconds(Math.round(hours * 3600));
+    }
+
+    @Override
+    public Resources getUpgradeCost() {
+        int metal = (int)Math.round(METAL_COST_VALUE * Math.pow(1.6, level));
+        int crystal = (int)Math.round(CRYSTAL_COST_VALUE * Math.pow(1.6, level));
+        int deuterium = (int)Math.round(DEUTERIUM_COST_VALUE * Math.pow(1.6, level));
+        return new Resources(metal, crystal, deuterium);
+    }
+
+    @Override
+    void updateDependencies() {
+        laboratory = (Laboratory)department.getBuilding(BuildingType.LABORATORY);
+    }
+}
