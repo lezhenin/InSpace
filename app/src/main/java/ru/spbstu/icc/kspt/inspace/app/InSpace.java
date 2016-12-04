5c153e9dd1965e570756bf92dc0b19edc12a12de
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2016-12-05 01:12:37 +0300

    add separator

--- app/src/main/java/ru/spbstu/icc/kspt/inspace/app/InSpace.java
+++ app/src/main/java/ru/spbstu/icc/kspt/inspace/app/InSpace.java
@@ -3,19 +3,26 @@ package ru.spbstu.icc.kspt.inspace.app;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import javafx.util.Duration;
+import jdk.nashorn.internal.runtime.OptimisticReturnFilters;
 import ru.spbstu.icc.kspt.inspace.model.Planet;
 import ru.spbstu.icc.kspt.inspace.model.Position;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class InSpace extends Application {
 
@@ -42,6 +49,7 @@ public class InSpace extends Application {
 
         root.add(getOverviewPane(10, 645), 1, 0, 1, menuButtons.size());
 
+
         Scene scene = new Scene(root, 800, 800);
 
         primaryStage.setTitle("My JavaFX Application");
@@ -60,31 +68,56 @@ public class InSpace extends Application {
         Text resources = new Text("Resources");
         resources.setTextAlignment(TextAlignment.CENTER);
         resources.setWrappingWidth(realWidth);
+        resources.setFont(new Font("Arial", 25));
+        overview.add(resources, 0, 0, 3, 1);
+
+        Text metal = new Text("Metal: " + planet.getResources().getMetal());
+        metal.setWrappingWidth(realWidth / 3);
+        metal.setTextAlignment(TextAlignment.CENTER);
+        overview.add(metal, 0, 1);
+
+        Text crystals = new Text("Crystals: " + planet.getResources().getCrystals());
+        crystals.setWrappingWidth(realWidth / 3);
+        crystals.setTextAlignment(TextAlignment.CENTER);
+        overview.add(crystals, 1, 1);
+
+        Text deuterium = new Text("Deuterium:  " + planet.getResources().getCrystals());
+        deuterium.setWrappingWidth(realWidth / 3);
+        deuterium.setTextAlignment(TextAlignment.CENTER);
+        overview.add(deuterium, 2, 1);
+
+        Separator separator = new Separator(Orientation.HORIZONTAL);
+        overview.add(separator, 0, 2, 3, 1);
 
         Text properties = new Text("Properties");
         properties.setWrappingWidth(realWidth);
         properties.setTextAlignment(TextAlignment.CENTER);
+        properties.setFont(new Font("Arial", 25));
+        overview.add(properties, 0, 3, 3, 1);
 
-        Text metal = new Text("Metal: " + planet.getResources().getMetal());
-        metal.setWrappingWidth(realWidth / 3);
+        overview.add(new Text("Name:"), 0, 4);
+        Text name = new Text(planet.getName());
+        overview.add(name, 1, 4);
+        Button rename = new Button("Rename");
+        rename.setOnAction((ActionEvent e) -> {
+            TextInputDialog dialog = new TextInputDialog("New name...");
+            dialog.setTitle("Rename Planet Dialog");
+            dialog.setContentText("Enter new name:");
 
-        Text crystals = new Text("Crystals: " + planet.getResources().getCrystals());
-        crystals.setWrappingWidth(realWidth / 3);
+            Optional<String> result = dialog.showAndWait();
+            result.ifPresent(r -> {
+                planet.rename(result.get());
+                name.setText(planet.getName());
+            });
 
-        Text deuterium = new Text("Deuterium:  " + planet.getResources().getCrystals());
-        deuterium.setWrappingWidth(realWidth / 3);
+        });
+        overview.add(rename, 2, 4);
 
-        overview.add(resources, 0, 0, 3, 1);
-        overview.add(metal, 0, 1);
-        overview.add(crystals, 1, 1);
-        overview.add(deuterium, 2, 1);
-        overview.add(properties, 0, 2, 3, 1);
-        overview.add(new Text("Name:"), 0, 3);
-        overview.add(new Text(planet.getName()), 1, 3);
-        overview.add(new Text("Number of fields:"), 0, 4);
-        overview.add(new Text(String.valueOf(planet.getNumberOfFields())), 1, 4);
-        overview.add(new Text("Number of empty fields:"), 0, 5);
-        overview.add(new Text(String.valueOf(planet.getNumberOfEmptyFields())), 1, 5);
+
+        overview.add(new Text("Number of fields:"), 0, 5);
+        overview.add(new Text(String.valueOf(planet.getNumberOfFields())), 1, 5);
+        overview.add(new Text("Number of empty fields:"), 0, 6);
+        overview.add(new Text(String.valueOf(planet.getNumberOfEmptyFields())), 1, 6);
 
         Timeline fiveSecondsWonder = new Timeline(new KeyFrame(Duration.seconds(10), event -> {
             System.out.println("upd");
@@ -97,6 +130,17 @@ public class InSpace extends Application {
         fiveSecondsWonder.play();
 
         return overview;
+    }
+
+    private ListView getBuildingsPane() {
+        ListView buildingList = new ListView();
+        buildingList.setCellFactory(new Callback<ListView, ListCell>() {
+            @Override
+            public ListCell call(ListView param) {
+                return null;
+            }
+        });
+        return buildingList;
     }
 
     @Override
