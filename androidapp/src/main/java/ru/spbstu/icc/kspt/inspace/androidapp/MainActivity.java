d59b0c2b0a45abf591a7ab6325ce792b61d41dad
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2017-05-16 00:20:07 +0300

    edit build settings

--- androidapp/src/main/java/ru/spbstu/icc/kspt/inspace/androidapp/MainActivity.java
+++ androidapp/src/main/java/ru/spbstu/icc/kspt/inspace/androidapp/MainActivity.java
@@ -2,6 +2,10 @@ package ru.spbstu.icc.kspt.inspace.androidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                try {
+                    String url = "https://frozen-stream-78027.herokuapp.com/planets/";
+
+                    RestTemplate restTemplate = new RestTemplate();
+                    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+                    String result = restTemplate.getForObject(url, String.class);
+                    Log.d("res", result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
