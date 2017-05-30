118aca7cb7e3c84bf2d17a0b7cd4294f2707e5c0
Yurij Lezhenin <yurij.lezhenin@yandex.ru>
2017-05-30 21:01:32 +0300

    rename

--- androidapp/src/main/java/ru/spbstu/icc/kspt/inspace/androidapp/UpgradableListFragment.java
+++ androidapp/src/main/java/ru/spbstu/icc/kspt/inspace/androidapp/UpgradableListFragment.java
@@ -2,7 +2,6 @@ package ru.spbstu.icc.kspt.inspace.androidapp;
 
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -102,7 +101,7 @@ public class UpgradableListFragment extends Fragment {
         public UpgradablesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                                 int viewType) {
             View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.building_list_item, parent, false);
+                    .inflate(R.layout.upgradables_list_item, parent, false);
 
             return new UpgradablesAdapter.ViewHolder(v);
         }
